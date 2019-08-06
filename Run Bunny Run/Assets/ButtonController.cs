dac2c48f7903df5ec04ba5b0fb525949a6adfc85
achuthanandGit <achuthanand.git@gmail.com>
2019-08-06 18:16:28 +1200

    refs #master adding custom fonts and UI

---
+++ Run Bunny Run/Assets/ButtonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    private InputField input;
+    private InputField output;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = this.GetComponent<InputField>();
+        output = this.GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetInputData()
+    {
+        input = GameObject.Find("inputUsername").GetComponent<InputField>();
+        string username = input.text;
+        output = GameObject.Find("outputField").GetComponent<InputField>();
+        output.text = username;
+        Debug.Log(username);
+    }
+}
